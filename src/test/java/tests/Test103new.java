e2d3e596c0cde5385e376ddf45a8be1d1f78b40c
Vasilyeva_N <nataliavasilyeva2018@gmail.com>
2018-10-09 17:37:56 +0300

    new tests: TestTourism, TestChildren

--- src/test/java/tests/Test103new.java
+++ src/test/java/tests/Test103new.java
@@ -22,33 +22,44 @@ public class Test103new {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
+        driver.get("https://www.103.by/");
     }
     @Test
     public void TestName() {
-        driver.get("https://www.103.by/");
         driver.findElement(By.cssSelector(".DropDown.Header__dropDown.DropDown--tick.DropDown--huge")).click();
         driver.findElement(By.className("Input__control")).sendKeys("Брест");
         driver.findElement(By.className("Radio__text")).click();
         Assert.assertEquals("Брест", driver.findElement(By.cssSelector(".DropDown__text.u-ellipsis")).getText());
     }
     @Test
-    public void TestTourism1() {
-        driver.get("https://www.103.by/");
+    public void TestJournal() {
        driver.findElement(By.linkText("Журнал")).click();
         Assert.assertEquals("Журнал 103.by", driver.getTitle());
     }
     @Test
+    public void TestChildren() {
+        driver.get("https://mag.103.by/main/");
+        driver.findElement(By.id("search_query")).sendKeys("Стоматология");
+        driver.findElement(By.linkText("Детская стоматология в Минске")).click();
+        Assert.assertEquals("Детский стоматолог в Минске, лечение в детских стоматологиях", driver.getTitle());
+    }
+    @Test
+    public void TestTourism() {
+        driver.get("https://mag.103.by/main/");
+        driver.findElement(By.xpath("//li[3]/span")).click();
+        driver.findElement(By.xpath("//a[contains(text(),'АГЕНТСТВА ПО МЕДТУРИЗМУ')]")).click();
+        Assert.assertEquals("Медицинский туризм и лечение в Беларуси. Агентства по медицинскому туризму в Белорусии", driver.getTitle());
+    }
+    @Test
     public void TestSearch() {
-        driver.get("https://www.103.by/");
         driver.findElement(By.id("search_open")).click();
-        driver.findElement(By.className("iSearch Search--inModal")).sendKeys("qqq");
+        driver.findElement(By.tagName("input ")).sendKeys("qqq");
     }
 
     @AfterMethod
 
     public void afterMethod() {
 
-       // driver.quit();
+        driver.quit();
     }
 }
